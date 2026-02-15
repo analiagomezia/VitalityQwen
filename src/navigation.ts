e5918f553acb5c9182c950dd422c3ca55b761e6d
Any <analiagomez.ia@gmail.com>
2026-02-15 05:49:09 -0300

    Cambios varios
    Estilos visuales, mobile, agregue links, boton de whatsapp...

--- src/navigation.ts
+++
@@ -1,7 +0,0 @@
-import { createNavigation } from 'next-intl/navigation';
-
-export const locales = ['es', 'en'] as const;
-export const localePrefix = 'always'; // Default: 'always'
-
-export const { Link, redirect, usePathname, useRouter } =
-    createNavigation({ locales, localePrefix });
